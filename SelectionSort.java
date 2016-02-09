92217b1d141e3d1ab3717bfc124987cae91f611c
anhadbhasin11 <anhad_bhasin@yahoo.co.in>
2016-02-08 19:15:27 -0800

    first commit3

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,15 +1,48 @@
-import java.util.*;
 
+/*
+ * Selection Sort is one of the easiest way to sort.
+ * Time Complexity : O(n^2)
+ * Space Complexity : O(1)
+ * In Place :  Yes
+ * 
+ * Anhad S Bhasin
+ */
 public class SelectionSort {
+	
+	public static void sort(int[] array)
+	{
+		for(int i = 0; i < array.length; ++i)
+		{
+			if(array[i] > array[minimum(array,i)])
+				swap(array,i,minimum(array,i));
+		}
+	}
+	private static int minimum(int[] array, int i)
+	{
+		int min = i;
+		for(int j = i; j < array.length; ++j)
+		{
+			if(array[j] < array[min])
+				min = j;
+		}
+		return min;
+	}
+	private static void swap (int[] array, int x, int y)
+	{
+		int swap = array[x];
+		array[x] = array[y];
+		array[y] = swap;
+	}
+	
 	
 	public static void main(String args[])
 	{
-		int[] arr = {5,6,1,4,2,8,9,0};
-		Arrays.sort(arr);
-		for(int i = 0;  i < arr.length; i++)
-		{
-			System.out.println(arr[i]);
-		}
+		int[] array = {0,9,8,5,4,7,1,2,50,100,1,6};
+		sort(array);
+		for(int i = 0; i < array.length; ++i)
+			System.out.println(array[i]);
+		
+		
 	}
 	
 
